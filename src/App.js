5207da9e85064f5a4b2814d41b7dd4fad6dfbf24
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-10 18:05:59 -0300

    feat: persist auth of user logged

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
@@ -8,18 +9,20 @@ import './Config/ReactotronConfig';
 import Routes from './Routes';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from '~/store/index';
 
 import GlobalStyle from './Styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-        <ToastContainer autoClose={3000} />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoClose={3000} />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
