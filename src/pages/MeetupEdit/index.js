83a535c8a62245d2c09691c3d416c22dbd12fedb
Juliano Krindges <juliano9krindges@hotmail.com>
2019-10-12 22:50:59 -0300

    chore: add and styled MeetupEdit screen

--- src/pages/MeetupEdit/index.js
+++ src/pages/MeetupEdit/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { MdLoyalty } from 'react-icons/md';
+import { Container } from './styles';
 
 export default function MeetupEdit() {
-  return <div />;
+  const dispatch = useDispatch();
+  const meetups = useSelector(state => state.meetup);
+
+  return (
+    <Container>
+      <Form>
+        <Input type="text" name="title" placeholder="Título do Meetup" />
+        <Input multiline name="description" placeholder="Descrição completa" />
+        <Input type="text" name="date" placeholder="Data do Meetup" />
+        <Input type="text" name="location" placeholder="Localização" />
+        <button type="submit">
+          <MdLoyalty size={20} color="#fff" /> Salvar Meetup
+        </button>
+      </Form>
+    </Container>
+  );
 }
